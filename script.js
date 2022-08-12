8c126d93055362c698af2b1ac804d7a4ed93365c
RafaelFreitas <raf.mec.ba@gmail.com>
2022-08-11 21:32:53 -0300

    S3-11 ENTREGA

---
+++ script.js
@@ -0,0 +1,10 @@
+let nome = "Rafael";
+let sobrenome = "Freitas";
+let anoNascimento = 1989;
+let idade = 2022 - anoNascimento
+let cidade = "Salvador";
+let nomeCompleto = `${nome} ${sobrenome}`
+
+console.log(`Eu sou o ${nomeCompleto} tenho ${idade} anos e moro em ${cidade}`)
+
+console.log(`Eu sou o ${nome} ${sobrenome} tenho ${idade} anos e moro em ${cidade}`)
